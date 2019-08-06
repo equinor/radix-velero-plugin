e8c18127f3413e30dea52ec57055f546318eb00c
INGE KNUDSEN <ingeknudsen@INGEs-MacBook-Pro.local>
2019-08-06 13:43:39 +0200

    Restore of jobs need to be in a separate plugin

--- velero-plugins/status/restore_deployment.go
+++ velero-plugins/status/restore_deployment.go
@@ -27,23 +27,23 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
-// RestorePlugin is a restore item action plugin for Velero
-type RestorePlugin struct {
+// RestoreDeploymentPlugin is a restore item action plugin for Velero
+type RestoreDeploymentPlugin struct {
 	Log logrus.FieldLogger
 }
 
 // AppliesTo returns information about which resources this action should be invoked for.
 // A RestoreItemAction's Execute function will only be invoked on items that match the returned
 // selector. A zero-valued ResourceSelector matches all resources.g
-func (p *RestorePlugin) AppliesTo() (velero.ResourceSelector, error) {
+func (p *RestoreDeploymentPlugin) AppliesTo() (velero.ResourceSelector, error) {
 	return velero.ResourceSelector{
-		IncludedResources: []string{"radixdeployments.radix.equinor.com", "radixjobs.radix.equinor.com"},
+		IncludedResources: []string{"radixdeployments.radix.equinor.com"},
 	}, nil
 }
 
 // Execute allows the RestorePlugin to perform arbitrary logic with the item being restored,
 // in this case, setting a custom annotation on the item being restored.
-func (p *RestorePlugin) Execute(input *velero.RestoreItemActionExecuteInput) (*velero.RestoreItemActionExecuteOutput, error) {
+func (p *RestoreDeploymentPlugin) Execute(input *velero.RestoreItemActionExecuteInput) (*velero.RestoreItemActionExecuteOutput, error) {
 	p.Log.Info("Radix Deployment RestorePlugin!")
 
 	metadata, err := meta.Accessor(input.Item)
---
+++ velero-plugins/status/restore_job.go
@@ -0,0 +1,74 @@
+/*
+Copyright 2018 the Heptio Ark contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package status
+
+import (
+	"encoding/json"
+
+	v1 "github.com/equinor/radix-operator/pkg/apis/radix/v1"
+	"github.com/heptio/velero/pkg/plugin/velero"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+// RestoreJobPlugin is a restore item action plugin for Velero
+type RestoreJobPlugin struct {
+	Log logrus.FieldLogger
+}
+
+// AppliesTo returns information about which resources this action should be invoked for.
+// A RestoreItemAction's Execute function will only be invoked on items that match the returned
+// selector. A zero-valued ResourceSelector matches all resources.g
+func (p *RestoreJobPlugin) AppliesTo() (velero.ResourceSelector, error) {
+	return velero.ResourceSelector{
+		IncludedResources: []string{"radixjobs.radix.equinor.com"},
+	}, nil
+}
+
+// Execute allows the RestorePlugin to perform arbitrary logic with the item being restored,
+// in this case, setting a custom annotation on the item being restored.
+func (p *RestoreJobPlugin) Execute(input *velero.RestoreItemActionExecuteInput) (*velero.RestoreItemActionExecuteOutput, error) {
+	p.Log.Info("Radix Job RestorePlugin!")
+
+	metadata, err := meta.Accessor(input.Item)
+	if err != nil {
+		return &velero.RestoreItemActionExecuteOutput{}, err
+	}
+
+	annotations := metadata.GetAnnotations()
+	if annotations == nil {
+		annotations = make(map[string]string)
+	}
+
+	var rj v1.RadixJob
+	if err := runtime.DefaultUnstructuredConverter.FromUnstructured(input.ItemFromBackup.UnstructuredContent(), &rj); err != nil {
+		return nil, errors.Wrap(err, "unable to convert unstructured item to Radix Deployment")
+	}
+
+	restoredStatus, err := json.Marshal(rj.Status)
+
+	if err != nil {
+		return &velero.RestoreItemActionExecuteOutput{}, err
+	}
+
+	annotations["equinor.com/velero-restored-status"] = string(restoredStatus)
+	metadata.SetAnnotations(annotations)
+
+	return velero.NewRestoreItemActionExecuteOutput(input.Item), nil
+}
