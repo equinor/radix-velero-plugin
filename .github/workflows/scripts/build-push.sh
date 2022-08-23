5d13eafe5087cfd48b9d33cbfc5985d13b27034d
Magnus Longva (IT MSS LID) <magnus@longva.email>
2022-08-23 12:55:44 +0200

    using ACR tasks for build

--- .github/workflows/scripts/build-push.sh
+++ .github/workflows/scripts/build-push.sh
@@ -6,9 +6,17 @@ build_id=${GITHUB_REF_NAME}-${sha}-${ts}
 
 image_build_tag=${ACR_NAME}.azurecr.io/${IMAGE_NAME}:$build_id
 image_latest_tag=${ACR_NAME}.azurecr.io/${IMAGE_NAME}:${GITHUB_REF_NAME}-latest
-
-docker build . -t $image_build_tag -t $image_latest_tag
-
-az acr login --name ${ACR_NAME}
-docker push $image_build_tag
-docker push $image_latest_tag
+az acr task run \
+    --subscription ${AZURE_SUBSCRIPTION_ID} \
+    --name radix-image-builder-internal \
+    --registry ${ACR_NAME} \
+    --context ${GITHUB_WORKSPACE} \
+    --file ${GITHUB_WORKSPACE}/Dockerfile \
+    --set DOCKER_REGISTRY=${ACR_NAME} \
+    --set BRANCH=${GITHUB_REF_NAME} \
+    --set TAGS="--tag ${image_build_tag} --tag ${image_latest_tag}" \
+    --set DOCKER_FILE_NAME=Dockerfile \
+    --set PUSH="--push" \
+    --set REPOSITORY_NAME=${IMAGE_NAME} \
+    --set CACHE="" \
+    --set CACHE_TO_OPTIONS="--cache-to=type=registry,ref=${ACR_NAME}.azurecr.io/${IMAGE_NAME}:radix-cache-${GITHUB_REF_NAME},mode=max"
